1459d4a02bc882c84e2cd0d461cf196ae0b9c662
Phyo Thandar Thant <phyothandarthant@example.com>
2021-09-14 18:09:23 +0900

    ユーザー表示画面追加

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,7 @@ Rails.application.routes.draw do
 
   get '/signup', to: 'user#new'
 
+  resources :user
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
