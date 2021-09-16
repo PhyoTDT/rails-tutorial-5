a376b1c8d5612cbb58b7f0f897e9548039da6c2d
Phyo Thandar Thant <phyothandarthant@example.com>
2021-09-16 18:59:43 +0900

    ログイン画面追加

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user =User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ログイン→ユーザー情報ページにダイレクト
+      log_in user
+      redirect_to user
+    else
+      #ログイン失敗→エーラメッセージを表示
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+    #
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-  root 'static_pages#home'
+  #get 'sessions/new'
 
-  #get 'static_pages/home'
+  root 'static_pages#home'
 
   get '/help', to: 'static_pages#help' #, as: 'helf'
 
@@ -13,6 +13,12 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
 
   post '/signup', to: 'users#create'
+
+  get    '/login',   to: 'sessions#new'
+
+  post   '/login',   to: 'sessions#create'
+  
+  delete '/logout',  to: 'sessions#destroy'
 
   resources :users
 
