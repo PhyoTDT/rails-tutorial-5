8e2c973e693d6acea82b64fa8737512c1f07530d
Phyo Thandar Thant <phyothandarthant@example.com>
2021-09-21 18:41:15 +0900

    Remember me チェックボックス追加追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # ログイン→ユーザー情報ページにダイレクト
       log_in user
-      remember user
+      params[:session][:remember_me] == '1'? remember(user) : forget(user)
       redirect_to user
     else
       #ログイン失敗→エーラメッセージを表示
