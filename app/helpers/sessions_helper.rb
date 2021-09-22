aadc121eb1b8ad4d3a45969db3c1314f48a6acd3
Phyo Thandar Thant <phyothandarthant@example.com>
2021-09-22 12:28:55 +0900

    Remember Me のテスト

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -16,8 +16,9 @@ module SessionsHelper
         if (user_id = session[:user_id])
             @current_user ||= User.find_by(id:user_id)
         elsif (user_id = cookies.signed[:user_id])   
+            #raise #TODO
             user = User.find_by(id:user_id)
-            if user && user.authenticated?(cookies[:remember_token])
+            if user && user.authenticated?(cookies[:remember_token]) # only if user portion, error will occurs with rails test
                 log_in user
                 @current_user = user
             end
