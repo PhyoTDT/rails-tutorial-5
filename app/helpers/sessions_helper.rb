143d60b11fb805daa19478c871ee730555ff8eb8
Phyo Thandar Thant <phyothandarthant@example.com>
2021-09-28 15:59:12 +0900

    アカウント有効化追加

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -23,7 +23,7 @@ module SessionsHelper
         elsif (user_id = cookies.signed[:user_id])   
             #raise #TODO
             user = User.find_by(id:user_id)
-            if user && user.authenticated?(cookies[:remember_token]) # only if user portion, error will occurs with rails test
+            if user && user.authenticated?(:remember, cookies[:remember_token]) # only if user portion, error will occurs with rails test
                 log_in user
                 @current_user = user
             end
